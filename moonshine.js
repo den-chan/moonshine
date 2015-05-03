b048b802433751970ac342b97771ba398c2afacc
den-chan <den-chan@tuta.io>
2015-05-03 19:50:53 +1000

    Two-page unicode handshake OK (Firefox & Chrome).

--- moonshine.js
+++ moonshine.js
@@ -30,21 +30,30 @@ $.merge($, {
   removeClass: function (el, cn) {
     el.className = el.className.replace( new RegExp("(?:^|\\s)" + cn + "(?!\\S)"), '' );
     return el
+  },
+  compress: function (s) {
+    s = btoa(s);
+    if (s.length % 2 !== 0) s += " ";
+    for (var i = 0, out = ""; i < s.length; i+=2) out += String.fromCharCode((s.charCodeAt(i) * 256) + s.charCodeAt(i + 1));
+    return out;
+  },
+  decompress: function (s) {
+    for (var i = 0, out = "", n; i < s.length; i++) {
+      n = s.charCodeAt(i);
+      out += String.fromCharCode(Math.floor(n / 256), n % 256)
+    }
+    return atob(out)
   }
 });
 
 function init() {
   var
-    nilfun = function () {},
     servers = $.ajax("stun.json"),
     pcConstraint = { optional: [{ "RtpDataChannels": false }] },
     dataConstraint = { reliable: false },
       
-    pc1 = new RTCPeerConnection(servers, pcConstraint),
-    pc2 = new RTCPeerConnection(servers, pcConstraint),
-    dc1 = null,
-    dc2 = null,
-    activedc,
+    pc = new RTCPeerConnection(servers, pcConstraint),
+    dc, nilfun = function () {},
 
     sendButton = $("#serial-chat button.send"),
     ticketTA = $.addEvent($("#serial-chat .ticket > textarea"), "click", function () { this.select() }),
@@ -55,37 +64,45 @@ function init() {
     exchangeView = $("#serial-chat > .exchange");
 
   $.addEvent($("#serial-chat .connect > .create"), "click", function () {
+    $.merge(pc, {
+      onicecandidate: function (e) {
+        if (e.candidate == null) {
+          ticketTA.value = $.compress(JSON.stringify(pc.localDescription));
+          ticketTA.select()
+        }
+      },
+      onconnection: dataChannelSend.focus
+    });
     $.addEvent(ticketTA, "cut copy", function () {
       $.addEvent(ticketTA, "paste", function () { setTimeout(function () {
-        var adesc = new RTCSessionDescription(JSON.parse(ticketTA.value));
-        pc1.setRemoteDescription(adesc)
+        var adesc = new RTCSessionDescription(JSON.parse($.decompress(ticketTA.value)));
+        pc.setRemoteDescription(adesc)
       }.bind(this), 0) });
       ticketInfo.innerHTML = "They need to give you their stub<br />Paste it here"
     });
     $.removeClass(exchangeView, "hide");
     $.addClass(connectView, "hide");
     ticketInfo.innerHTML = "This is a ticket to enter<br />Pass it on";
-    
-    activedc = dc1 = $.merge(pc1.createDataChannel('test', {reliable: true}), {
-      onopen: function (e) {
-        $.addClass(serialChat, "open");
-        $.addClass(exchangeView, "hide");
-        $.addEvent(sendButton, "click", sendMessage)
-      },
-      onmessage: function (e) {
-        if (e.data.charCodeAt(0) == 2) return;
-        var data = JSON.parse(e.data);
-        writeToChatLog(data.message, "bob")
-      }
-    });
-    pc1.createOffer(function (desc) { pc1.setLocalDescription(desc, nilfun, nilfun) }, nilfun)
+    dc = $.merge( pc.createDataChannel('test', {reliable: true}), { onopen: openChannel, onmessage: getMessage } );
+    pc.createOffer(function (desc) { pc.setLocalDescription(desc, nilfun, nilfun) }, nilfun)
   });
   
   $.addEvent($("#serial-chat .connect > .redeem"), "click", function () {
+    $.merge(pc, {
+      ondatachannel: function (e) { dc = $.merge(e.channel || e, {onopen: openChannel, onmessage: getMessage }) },
+      onicecandidate: function (e) {
+        if (e.candidate == null) {
+          ticketInfo.innerHTML = "Here is your ticket stub<br />Pass it back";
+          ticketTA.value = $.compress(JSON.stringify(pc.localDescription));
+          ticketTA.select()
+        }
+      },
+      onconnection: dataChannelSend.focus
+    });
     $.addEvent(ticketTA, "paste", function () { setTimeout(function () {
-      var odesc = new RTCSessionDescription(JSON.parse(ticketTA.value));
-      pc2.setRemoteDescription(odesc, function () {
-        pc2.createAnswer( function (adesc) { pc2.setLocalDescription(adesc) }, nilfun )
+      var odesc = new RTCSessionDescription(JSON.parse($.decompress(ticketTA.value)));
+      pc.setRemoteDescription(odesc, function () {
+        pc.createAnswer( function (adesc) { pc.setLocalDescription(adesc) }, nilfun )
       }, nilfun)
     }.bind(this), 0) });
     $.removeClass(exchangeView, "hide");
@@ -93,44 +110,27 @@ function init() {
     ticketInfo.innerHTML = "You need a ticket to enter<br />Paste it here";
     ticketTA.focus()
   });
-
-  $.merge(pc1, {
-    onicecandidate: function (e) {
-      if (e.candidate == null) {
-        ticketTA.value = JSON.stringify(pc1.localDescription);
-        ticketTA.select()
-      }
-    },
-    onconnection: dataChannelSend.focus
-  });
-
-  $.merge(pc2, {
-    ondatachannel: function (e) {
-      dc2 = activedc = e.channel || e;
-      dc2.onopen = function (e) {
-        $.addClass(serialChat, "open");
-        $.addClass(exchangeView, "hide");
-        $.addEvent(sendButton, "click", sendMessage)
-      };
-      dc2.onmessage = function (e) {
-        var data = JSON.parse(e.data);
-        writeToChatLog(data.message, "bob")
-      }
-    },
-    onicecandidate: function (e) {
-      if (e.candidate == null) {
-        ticketInfo.innerHTML = "Here is your ticket stub<br />Pass it back";
-        ticketTA.value = JSON.stringify(pc2.localDescription);
-        ticketTA.select()
-      }
-    },
-    onconnection: dataChannelSend.focus
-  })
+  
+  function openChannel() {
+    $.addClass(serialChat, "open");
+    $.addClass(exchangeView, "hide");
+    $.addEvent(sendButton, "click", sendMessage);
+    $.addEvent(dataChannelSend, "keyup", function (e) {
+      if (e.keyCode == 13 && e.ctrlKey === true) sendMessage();
+      return false
+    })
+  }
+  
+  function getMessage(e) {
+    if (e.data.charCodeAt(0) == 2) return;
+    var data = JSON.parse(e.data);
+    writeToChatLog("[Remote] " + data.message, "bob")
+  }
   
   function sendMessage(channel) {
     if (dataChannelSend.value) {
-      writeToChatLog(dataChannelSend.value, "alice");
-      activedc.send( JSON.stringify({ message: dataChannelSend.value }) );
+      writeToChatLog("[Local] " + dataChannelSend.value, "alice");
+      dc.send( JSON.stringify({ message: dataChannelSend.value }) );
       dataChannelSend.value = ""
     }
   }
