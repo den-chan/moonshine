0e8bf8871f9cb16af8de7ee2b167720f220270c3
den-chan <den-chan@tuta.io>
2015-05-05 04:42:17 +1000

    Two user basic anonymous BBS + partial CSS design.

--- moonshine.js
+++ moonshine.js
@@ -1,7 +1,7 @@
 RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
 RTCSessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
 
-$ = function (sel, a) { return (a = [].slice.call( document.querySelectorAll(sel) )).length > 1 ? a : a[0] };
+$ = function (sel, a) { return (a = [].slice.call( (this == window ? document : this).querySelectorAll(sel) )).length > 1 ? a : a[0] };
 $.merge = function (obj1, obj2) {
   for (var a in obj2) obj1[a] = obj2[a];
   return obj1
@@ -31,8 +31,13 @@ $.merge($, {
     el.className = el.className.replace( new RegExp("(?:^|\\s)" + cn + "(?!\\S)"), '' );
     return el
   },
+  addAttr: function (el, ah) { for (var a in ah) el.setAttribute(a, ah[a]) },
+  insert: function (pe, ce, c, fn) {
+    var j = pe.childNodes.length;
+    while (j > 0 && fn(ce.getAttribute(c), pe.childNodes[j - 1].getAttribute(c))) j--;
+    j == pe.childNodes.length ? pe.appendChild(ce) : pe.insertBefore(ce, pe.childNodes[j])
+  },
   compress: function (s) {
-    s = btoa(s);
     if (s.length % 2 !== 0) s += " ";
     for (var i = 0, out = ""; i < s.length; i+=2) out += String.fromCharCode((s.charCodeAt(i) * 256) + s.charCodeAt(i + 1));
     return out;
@@ -42,7 +47,17 @@ $.merge($, {
       n = s.charCodeAt(i);
       out += String.fromCharCode(Math.floor(n / 256), n % 256)
     }
-    return atob(out)
+    return out
+  },
+  hash: function(s) {
+    var hash = 0, i, chr, len;
+    if (s.length == 0) return hash;
+    for (i = 0, len = s.length; i < len; i++) {
+      chr = s.charCodeAt(i);
+      hash = ((hash << 5) - hash) + chr >>> 0;
+    }
+    hs = hash.toString(16);
+    return Array(9 - hs.length).join("0") + hs
   }
 });
 
@@ -52,26 +67,32 @@ function init() {
     pcConstraint = { optional: [{ "RtpDataChannels": false }] },
     dataConstraint = { reliable: false },
       
-    pc = new RTCPeerConnection(servers, pcConstraint),
-    dc, nilfun = function () {},
+    pc, dc, nilfun = function () {},
+    action, pageState = {},
 
-    sendButton = $("#serial-chat button.send"),
-    ticketTA = $.addEvent($("#serial-chat .ticket > textarea"), "click", function () { this.select() }),
-    ticketInfo = $("#serial-chat .info > span"),
-    dataChannelSend = $("#serial-chat textarea.message"),
-    serialChat = $("#serial-chat"),
-    connectView = $("#serial-chat > .connect"),
-    exchangeView = $("#serial-chat > .exchange");
+    sendButton = $("#room button.send"),
+    ticketTA = $.addEvent($("#exchange > .ticket > textarea"), "click", function () { this.select() }),
+    ticketInfo = $("#exchange > .info > span");
+  
+  $.addEvent($("body"), "beforeunload", closeChannel);
+  $.addEvent($("#room > .close"), "click", closeChannel);
 
-  $.addEvent($("#serial-chat .connect > .create"), "click", function () {
-    $.merge(pc, {
+  $.addEvent($("#room > button.send"), "click", sendMessage);
+  $.addEvent($("#room > textarea.message"), "keyup", function (e) {
+    if (e.keyCode == 13 && e.ctrlKey === true) sendMessage.bind(e.target)();
+    return false
+  });
+  
+  $.addEvent($("#connect > .create"), "click", function () {
+    ticketTA.innerHTML = "";
+    $.merge(pc = new RTCPeerConnection(servers, pcConstraint), {
       onicecandidate: function (e) {
         if (e.candidate == null) {
           ticketTA.value = $.compress(JSON.stringify(pc.localDescription));
           ticketTA.select()
         }
       },
-      onconnection: dataChannelSend.focus
+      onsignalingstatechange: function () { pc.signalingState != "closed" || closeChannel() }
     });
     $.addEvent(ticketTA, "cut copy", function () {
       $.addEvent(ticketTA, "paste", function () { setTimeout(function () {
@@ -80,15 +101,16 @@ function init() {
       }.bind(this), 0) });
       ticketInfo.innerHTML = "They need to give you their stub<br />Paste it here"
     });
-    $.removeClass(exchangeView, "hide");
-    $.addClass(connectView, "hide");
+    $.removeClass($("#exchange"), "hide");
+    $.addClass($("#connect"), "hide");
     ticketInfo.innerHTML = "This is a ticket to enter<br />Pass it on";
-    dc = $.merge( pc.createDataChannel('test', {reliable: true}), { onopen: openChannel, onmessage: getMessage } );
+    dc = $.merge( pc.createDataChannel('moonConn', {reliable: true}), { onopen: openChannel, onmessage: getMessage } );
     pc.createOffer(function (desc) { pc.setLocalDescription(desc, nilfun, nilfun) }, nilfun)
   });
   
-  $.addEvent($("#serial-chat .connect > .redeem"), "click", function () {
-    $.merge(pc, {
+  $.addEvent($("#connect > .redeem"), "click", function () {
+    ticketTA.innerHTML = "";
+    $.merge(pc = new RTCPeerConnection(servers, pcConstraint), {
       ondatachannel: function (e) { dc = $.merge(e.channel || e, {onopen: openChannel, onmessage: getMessage }) },
       onicecandidate: function (e) {
         if (e.candidate == null) {
@@ -97,7 +119,7 @@ function init() {
           ticketTA.select()
         }
       },
-      onconnection: dataChannelSend.focus
+      onsignalingstatechange: function () { pc.signalingState != "closed" || closeChannel() }
     });
     $.addEvent(ticketTA, "paste", function () { setTimeout(function () {
       var odesc = new RTCSessionDescription(JSON.parse($.decompress(ticketTA.value)));
@@ -105,40 +127,88 @@ function init() {
         pc.createAnswer( function (adesc) { pc.setLocalDescription(adesc) }, nilfun )
       }, nilfun)
     }.bind(this), 0) });
-    $.removeClass(exchangeView, "hide");
-    $.addClass(connectView, "hide");
+    $.removeClass($("#exchange"), "hide");
+    $.addClass($("#connect"), "hide");
     ticketInfo.innerHTML = "You need a ticket to enter<br />Paste it here";
     ticketTA.focus()
   });
   
   function openChannel() {
-    $.addClass(serialChat, "open");
-    $.addClass(exchangeView, "hide");
-    $.addEvent(sendButton, "click", sendMessage);
-    $.addEvent(dataChannelSend, "keyup", function (e) {
-      if (e.keyCode == 13 && e.ctrlKey === true) sendMessage();
-      return false
-    })
+    $.addClass($("#page"), "open");
+    $.addClass($("#exchange"), "hide");
+    $("#room > input.message-title").focus()
+  }
+  
+  function closeChannel () {
+    $.removeClass($("#page"), "open");
+    $.removeClass($("#connect"), "hide");
+    $("#room > .conversation").innerHTML = "";
+    if (pc.signalingState != "closed") {
+      action == "closed" || dc.send( JSON.stringify({ action: "closed" }) );
+      pc.close()
+    }
   }
   
   function getMessage(e) {
     if (e.data.charCodeAt(0) == 2) return;
     var data = JSON.parse(e.data);
-    writeToChatLog("[Remote] " + data.message, "bob")
+    if (data.update) { updatePage(data.update) }
+    else if (data.action && (action = data.action) == "closed") { closeChannel() } //TODO: test for abrupt close
   }
   
-  function sendMessage(channel) {
-    if (dataChannelSend.value) {
-      writeToChatLog("[Local] " + dataChannelSend.value, "alice");
-      dc.send( JSON.stringify({ message: dataChannelSend.value }) );
-      dataChannelSend.value = ""
-    }
+  function sendMessage() { //check message length
+    var ta, payload, ts = Date.now();
+    if (this.parentNode.id == "room" && (ta = $("#room > textarea.message")).value) {
+      updatePage(payload = {
+        items: [{
+          id: "item-" + $.hash(ts + ta.value),
+          timestamp: ts,
+          title: $.bind(this.parentNode)("input.message-title").value,
+          body: ta.value
+        }]
+      });
+      $.bind(this.parentNode)("input.message-title").value = ""
+    } else if (this.parentNode.className == "page-item" && (ta = $.bind(this.parentNode)("textarea.message")).value) {
+      updatePage(payload = {
+        items: [{
+          id: ta.parentNode.id,
+          comments: [{
+            id: "comment-" + $.hash(ts + ta.value),
+            timestamp: ts,
+            body: ta.value
+          }]
+        }]
+      })
+    } else { return false }
+    dc.send( JSON.stringify({update: payload}) );
+    ta.value = ""
   }
 
-  function writeToChatLog(message, message_type) {
-    var msg = $("#stamps > .chat-message").cloneNode(true);
-    msg.innerHTML = message;
-    $("#serial-chat div.conversation").appendChild($.addClass(msg, message_type))
+  function updatePage(payload) { //TODO jQuery
+    for (var i = 0, iE, c, j; i < payload.items.length; i++) {
+      payload.items[i].comments = payload.items[i].comments || [];
+      iE =
+        $("#" + payload.items[i].id) ||
+        (function (iE_) {$.bind(iE)("textarea.message")
+          $.addAttr(iE_, {id: payload.items[i].id, timestamp: payload.items[i].timestamp});
+          $.bind(iE_)(".title").innerHTML = payload.items[i].title;
+          $.bind(iE_)(".body").innerHTML = payload.items[i].body;
+          return iE_
+        })($("#stamps > .page-item").cloneNode(true));
+      for (c = 0; c < payload.items[i].comments.length; c++) {
+        $.insert($.bind(iE)(".comments"), (function (cE_) {
+          $.addAttr(cE_, {id: payload.items[i].comments[c].id, timestamp: payload.items[i].comments[c].timestamp}); //TODO user_data
+          $.bind(cE_)("span").innerHTML = payload.items[i].comments[c].body;
+          return cE_
+        })($("#stamps > .item-comment").cloneNode(true)), "timestamp", function (a, b) {return a < b})
+      }
+      $.insert($("#room > .conversation"), iE, "timestamp", function (a, b) {return a > b});
+      $.addEvent($.bind(iE)("button.send"), "click", sendMessage);
+      $.addEvent($.bind(iE)("textarea.message"), "keyup", function (e) {
+        if (e.keyCode == 13 && e.ctrlKey === true) sendMessage.bind(e.target)();
+        return false
+      });
+    }
   }
 
 }
